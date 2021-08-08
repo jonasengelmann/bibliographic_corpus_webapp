eac2c3ffd8d9db73eb70e36ff0ab6ffc9e659589
jonasengelmann <jonenone+github@gmail.com>
2021-08-08 18:43:50 +0200

    added view history view

--- app/corpus/views.py
+++ app/corpus/views.py
@@ -85,6 +85,72 @@ def get_resource_data(iri, key_order):
     }
 
 
+def get_history_data(iri):
+    query = f"""
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX oco: <https://w3id.org/oc/ontology/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        SELECT *
+        FROM <{iri}/prov>
+        FROM <{BASE_IRI}/prov/pa>
+        WHERE {{
+        ?ca a prov:Activity .
+        ?ca a ?type .
+        ?ca dcterms:description ?description .
+        ?se prov:wasGeneratedBy ?ca .
+        ?se prov:generatedAtTime ?generated_time .
+        ?ca prov:qualifiedAssociation ?curator .
+        ?curator prov:hadRole oco:occ-curator .
+        ?curator prov:agent ?curator_agent .
+        ?curator_agent foaf:name ?curator_name .
+        OPTIONAL {{
+            ?ca prov:qualifiedAssociation ?metadata_provider .
+            ?metadata_provider prov:hadRole
+                oco:source-metadata-provider .
+            ?metadata_provider prov:agent ?metadata_provider_agent .
+            ?metadata_provider_agent foaf:name ?metadata_provider_name .
+        }}
+        OPTIONAL {{?se oco:hasUpdateQuery ?update_query . }}
+        OPTIONAL {{?ca prov:used ?used_resource . }}
+        OPTIONAL {{?ca prov:wasInformedBy ?informed_ca .}}
+        FILTER(?type != prov:Activity)
+    }}
+    """
+    result = sparqlstore.query(query)
+
+    view_history_objects = []
+    for x in result:
+        view_history_object = {
+            'Type': {'name': vocabularies[x['type']['value']], 'link': x['ca']['value']},
+            'Generated At Time': {'name': x['generated_time']['value'], 'link': None},
+            'Description': {'name': x['description']['value'], 'link': None},
+            'Curator': {'name': x['curator_name']['value'], 'link': x['curator_agent']['value']},
+        }
+        if x.get('metadata_provider'):
+            view_history_object['Metadata Provider'] = {
+                'name': x['metadata_provider_name']['value'],
+                'link': x['metadata_provider_agent']['value']
+            }
+        if x.get('informed_ca'):
+            view_history_object['Informed By'] = {
+                'name': fromat_as_short_iri(x['informed_ca']['value']),
+                'link': x['informed_ca']['value']
+            }
+        if x.get('used_resource'):
+            view_history_object['Used Ressource'] = {
+                'name': fromat_as_short_iri(x['used_resource']['value']),
+                'link': x['used_resource']['value']
+            }
+        if x.get('primary_resource'):
+            view_history_object['Primary Resource'] = {
+                'name': x['primary_resource']['value'],
+                'link': None,
+            }
+        view_history_objects.append(view_history_object)
+    return view_history_objects
+
+
 def generate_resource_view(iri, key_order):
     resource_data = get_resource_data(iri=iri, key_order=key_order)
     title = resource_data.pop('Label')[0]['name']
@@ -122,4 +188,24 @@ def prov_resource2(resource_dataset_id, resource_iri_count, dataset_id, iri_coun
     return generate_resource_view(
         iri=f'{BASE_IRI}/{resource_dataset_id}/{resource_iri_count}/prov/{dataset_id}/{iri_count}',
         key_order=property_orders[dataset_id]
+    )
+
+
+@blueprint.route('<dataset_id>/<iri_count>/view_history')
+def resource_with_history(dataset_id, iri_count):
+    resource_data = get_resource_data(
+        iri=f'{BASE_IRI}/{dataset_id}/{iri_count}',
+        key_order=property_orders[dataset_id]
+    )
+    title = resource_data.pop('Label')[0]['name']
+
+    history_data = get_history_data(
+        iri=f'{BASE_IRI}/{dataset_id}/{iri_count}'
+    )
+
+    return render_template(
+        'corpus/resource_with_history.html',
+        title=title,
+        data=resource_data,
+        history_data=history_data,
     )
